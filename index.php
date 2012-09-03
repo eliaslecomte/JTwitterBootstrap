0e0efdf142be3f8db759612cda3ba465989b0149
Elias Lecomte <elias.lecomte@gmail.com>
2012-09-03 20:46:36 +0200

    added google analytics

--- index.php
+++ index.php
@@ -30,6 +30,20 @@ $app = JFactory::getApplication();
 
 		<jdoc:include type="head" />
 
+		<?php if (!empty($this->params->get('GoogleAnalyticsId'))):?>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '<?php echo $this->params->get('GoogleAnalyticsId') ?>']);
+			_gaq.push(['_trackPageview']);
+		
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+		<?php endif; ?>
+
 	</head>
 
     <body data-spy="scroll" data-target=".bs-docs-sidebar">
